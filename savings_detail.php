aa1a18c532be982ec67fc652b08f84e688169074
Jason Balliet <jballiet@thunderhead.com>
2016-03-25 15:19:34 -0600

    added default store locations feature

--- savings_detail.php
+++ savings_detail.php
@@ -184,13 +184,13 @@
 						   <div class="col-sm-11 col-sm-offset-1">
 							   <!-- ONE Connect optimisation targer -->
 							   <div id="TH_MyAccounts_FeatureBox3">
-							   		<div class="well wbFeatureBox wbBackgroundGrey">
-                						<h3 class="text-left wbItemTitleNoIcon">Support</h3>
-										<p>&nbsp; &nbsp;• &nbsp; Rates and Fees<br>
-											&nbsp; &nbsp;• &nbsp; Online and telephone banking<br></p>
+									<div class="well wbFeatureBox wbBackgroundBlue" style="padding: 0px 10px 13px 10px">
+										<div id="featureIcon" style="float: left;margin-top: 5px"><img src="themes/wealth_banking_theme/images/icons/icon-location.png" width="100px" height="121px"></div>
+										<div id="featureContent" style="margin-left: 110px">
+											<h3 class="text-left wbItemTitleNoIcon">Store Locations</h3>
+											<p class="text-centered">Let us help you find a Customer 1st Bank in your area or current location.</p>
 										</div>
-								<!-- /ONE Connect -->
-								</div>
+									</div>
 							</div>
 						</div>
                 		<div class="row clearfix">
