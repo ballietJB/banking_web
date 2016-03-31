479a54ddf504d3e418e823c2dcbe7aaba1877962
Jason Balliet <jballiet@thunderhead.com>
2016-03-31 04:54:12 -0600

    new images

---
+++ samples/Other-Feature-Box.php
@@ -0,0 +1,52 @@
+<html>
+	<head>
+		<!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
+		<!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
+		<!--script src="js/less-1.3.3.min.js"></script-->
+		<!--append ‘#!watch’ to the browser URL, then refresh the page. -->
+
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400" type="text/css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:700" type="text/css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:800" type="text/css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" type="text/css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:700" type="text/css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Six+Caps" type="text/css">
+			
+		<link href="../themes/wealth_banking_theme/css/bootstrap.css" rel="stylesheet"></link>
+		<link href="../themes/wealth_banking_theme/css/style.css" rel="stylesheet"></link>
+		<link href="../themes/wealth_banking_theme/css/social-buttons.css" rel="stylesheet"></link>
+		<link href="../themes/wealth_banking_theme/css/font-awesome.css" rel="stylesheet"></link>
+
+		<!-- Fav and touch icons -->
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="../themes/wealth_banking_theme/img/apple-touch-icon-144-precomposed.png"></link>
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="../themes/wealth_banking_theme/img/apple-touch-icon-114-precomposed.png"></link>
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="../themes/wealth_banking_theme/img/apple-touch-icon-72-precomposed.png"></link>
+		<link rel="apple-touch-icon-precomposed" href="../themes/wealth_banking_theme/img/apple-touch-icon-57-precomposed.png"></link>
+		<link rel="shortcut icon" href="../themes/wealth_banking_theme/img/favicon.ico"></link>
+		<script type="text/javascript" src="../themes/wealth_banking_theme/js/jquery.min.js"></script>
+		<script type="text/javascript" src="../themes/wealth_banking_theme/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="../themes/wealth_banking_theme/js/scripts.js"></script><!-- /HEAD BLOCK ================================================= -->
+	</head>
+	<body>
+		<div class="container">
+			<!-- Preferred Checking Account -->
+			<div >
+				<div class="well wbFeatureBox wbBackgroundBlack" style="padding: 0px 10px 13px 10px">
+					<div id="featureIcon" style="float: left;margin-top: 15px">
+						<span title="REASON: Is existing savings account customer ; Had large single deposit or existnig balance; Has standard checking OR Is not existing Checking customer">
+							<img src="../themes/wealth_banking_theme/images/logo/ONE-white.png" width="70px" height="112px">
+						</span>
+					</div>
+					<div id="featureContent" style="margin-left: 85px">
+						<h3 class="text-left wbItemTitleNoIcon">Preferred Checking</h3>
+						<p class="text-centered">Preferred Checking is a great choice if you want to earn interest on your checking account. Plus you get quick and convenient online and mobile access to your funds</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+
+
+
+
